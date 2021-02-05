b35440a8a69086cd982bfc36a6f7ad30d3197669
fgezer <67882585+fgezer@users.noreply.github.com>
2021-02-05 00:27:28 +0000

    Add files via upload

---
+++ lift_sim_bei.R
@@ -0,0 +1,22 @@
+library(spatstat)
+
+load(url('https://github.com/fgezer/ens_models/raw/main/bei_data.RData'))
+source(url('https://github.com/fgezer/ens_models/raw/main/lift_sim.R'))
+
+L1 = lift2D(x = bei$x[1:100], y = bei$y[1:100], f = elev[1:100], nleft = 20, stage = NULL, 
+            keepnbrs = F, Lmat = T, rw = c(0,1000,0,500), method = 'convex')
+
+L2 = lift2D(x = bei$x[1:100], y = bei$y[1:100], f = elev[1:100], nleft = 20, stage = NULL, 
+            keepnbrs = F, Lmat = T, rw = c(0,1000,0,500), method = 'rectangle')
+
+L3 = lift2D(x = bei$x[1:100], y = bei$y[1:100], f = elev[1:100], nleft = 20, stage = NULL, 
+            keepnbrs = F, Lmat = T, rw = c(0,1000,0,500), method = 'double')
+
+L4 = lift2D(x = bei$x[1:100], y = bei$y[1:100], f = elev[1:100], nleft = 20, stage = NULL, 
+            keepnbrs = F, Lmat = T, rw = c(0,1000,0,500), method = 'base')
+
+L5 = lift2D(x = bei$x[1:100], y = bei$y[1:100], f = elev[1:100], nleft = 20, stage = NULL, 
+            keepnbrs = F, Lmat = T, rw = c(0,1000,0,500), method = 'augm')
+
+
+save(L1, L2, L3, L4, L5, file = 'out_bei.RData')
